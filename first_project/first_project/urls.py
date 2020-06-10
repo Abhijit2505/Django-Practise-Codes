4002ded6e3a1ee071dd02f71399cf24c001a2b01
Abhijit2505 <abhijittripathy99@gmail.com>
2020-06-10 08:42:13 +0530

    Added updated url and view

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from first_app import views
 
 urlpatterns = [
     path('',views.index,name = 'index'),
+    path('first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
 ]
